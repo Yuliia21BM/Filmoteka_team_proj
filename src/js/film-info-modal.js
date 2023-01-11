14dfd6efaccc9da57e820bdd7eaa23e40b2b1eda
Sergey <sergey.cherenkov96@gmail.com>
2023-01-11 21:35:33 +0200

    + getGener

--- src/js/film-info-modal.js
+++ src/js/film-info-modal.js
@@ -10,35 +10,39 @@ filmCardSection.addEventListener('click', (e) => {
     };
 
     searchMovieById(77).then(movieInfo => {
-        const POSTER_URL = 'https://image.tmdb.org/t/p/w500'
+        const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
+        const getGener = movieInfo.genres.map(gener => gener.name).join(', ');
+
         const createFilmModalMarkup = basicLightbox.create(`
         <div class="film-modal">
-        <div class="film-modal__wrapper">
-            <img class="film-modal__poster" src="${POSTER_URL + movieInfo.poster_path}" alt="Movie ${movieInfo.title} poster" width=264/>
-            <h2 class="film-modal__title">${movieInfo.title}</h2>
-            <ul class="film-modal__info-list">
-            <li class="film-modal__info-item">
-                <h3 class="film-modal__info-title">Vote / Votes</h3>
-                <p class="film-modal__info-text">${movieInfo.vote_average}/${movieInfo.vote_count}</p>
-            </li>
-            <li class="film-modal__info-item">
-                <h3 class="film-modal__info-title">Popularity</h3>
-                <p class="film-modal__info-text">${movieInfo.popularity}</p>
-            </li>
-            <li class="film-modal__info-item">
-                <h3 class="film-modal__info-title">Original Title</h3>
-                <p class="film-modal__info-text">${movieInfo.original_title}</p>
-            </li>
-            <li class="film-modal__info-item">
-                <h3 class="film-modal__info-title">Genre</h3>
-                <p class="film-modal__info-text"></p>
-            </li>
-            <li>
-                <h3 class="film-modal__info-item--about">About</h3>
-                <p class="film-modal__info-text--about">${movieInfo.overview}</p>
-            </li>
-            </ul>
-        </div>
+            <div class="film-modal__wrapper">
+                <img class="film-modal__poster" src="${POSTER_URL + movieInfo.poster_path}" alt="Movie ${movieInfo.title} poster" width=264/>
+                <div>
+                    <h2 class="film-modal__title">${movieInfo.title}</h2>
+                    <ul class="film-modal__info-list">
+                    <li class="film-modal__info-item">
+                        <h3 class="film-modal__info-title">Vote / Votes</h3>
+                        <p class="film-modal__info-text">${movieInfo.vote_average}/${movieInfo.vote_count}</p>
+                    </li>
+                    <li class="film-modal__info-item">
+                        <h3 class="film-modal__info-title">Popularity</h3>
+                        <p class="film-modal__info-text">${movieInfo.popularity}</p>
+                    </li>
+                    <li class="film-modal__info-item">
+                        <h3 class="film-modal__info-title">Original Title</h3>
+                        <p class="film-modal__info-text">${movieInfo.original_title}</p>
+                    </li>
+                    <li class="film-modal__info-item">
+                        <h3 class="film-modal__info-title">Genre</h3>
+                        <p class="film-modal__info-text">${getGener}</p>
+                    </li>
+                    <li>
+                        <h3 class="film-modal__info-item--about">About</h3>
+                        <p class="film-modal__info-text--about">${movieInfo.overview}</p>
+                    </li>
+                    </ul>
+                </div>
+            </div>
         </div>`);
         
         createFilmModalMarkup.show();
