17922968462c349e3b50d800046a218a837c93a9
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-12 19:30:53 +0200

    Add library js

--- src/library.js
+++ src/library.js
@@ -1,9 +1,8 @@
 import './js/config';
-import './js/upcoming-films';
+// import './js/upcoming-films';
 import './js/all-info-API';
 import './js/authentication-modal';
 import './js/authorization-modal';
-import './js/film-info-modal';
 import './js/firebase-author-authen-API';
 import './js/film-search-header';
 import './js/pagination';
@@ -12,3 +11,4 @@ import './js/youtube-info-API';
 import './js/trailer-modal';
 import './js/team-modal';
 import './js/BtnScrollToTop';
+// import './js/film-info-modal';
