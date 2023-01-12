52fb7786405da7f9b5fe501ca49db298a57092e5
Sergey <sergey.cherenkov96@gmail.com>
2023-01-12 18:43:24 +0200

    add btns to modal film

--- src/js/film-info-modal.js
+++ src/js/film-info-modal.js
@@ -28,30 +28,26 @@ export function openModal(e, childClass) {
     const createFilmModalMarkup = basicLightbox.create(
       `
         <div class="film-modal">
+              <button class="film-modal__close-btn" type="button">
+               <svg class="film-modal__close-btn-icon"width="30" height="30">
+                <use href="/src/images/svg/symbol-defs.svg#icon-close-modal-film-icon"></use>
+               </svg>
+            </button>
             <div class="film-modal__wrapper">
-                <img class="film-modal__poster" src="${
-                  POSTER_URL + movieInfo.poster_path
-                }" alt="Movie ${movieInfo.title} poster" />
-                <div>
+                <img class="film-modal__poster" src="${POSTER_URL + movieInfo.poster_path}" alt="Movie ${movieInfo.title} poster" />
                     <h2 class="film-modal__title">${movieInfo.title}</h2>
                     <ul class="film-modal__info-list">
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Vote / Votes</h3>
-                            <p class="film-modal__info-text">${
-                              movieInfo.vote_average
-                            }/${movieInfo.vote_count}</p>
+                            <p class="film-modal__info-text">${movieInfo.vote_average}/${movieInfo.vote_count}</p>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Popularity</h3>
-                            <p class="film-modal__info-text">${
-                              movieInfo.popularity
-                            }</p>
+                            <p class="film-modal__info-text">${movieInfo.popularity}</p>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Original Title</h3>
-                            <p class="film-modal__info-text">${
-                              movieInfo.original_title
-                            }</p>
+                            <p class="film-modal__info-text">${movieInfo.original_title}</p>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Genre</h3>
@@ -60,7 +56,10 @@ export function openModal(e, childClass) {
                     </ul>
                     <h3 class="film-modal__title-about">About</h3>
                     <p class="film-modal__text-about">${movieInfo.overview}</p>
-                </div>
+                <div class="film-modal__add-btns-wrapper">
+                <button class="film-modal__add-btns" type="button">Add to Watched</button>
+                <button class="film-modal__add-btns film-modal__add-btns--seconadry-btn" type="button">Add to queue</button>
+            </div>
             </div>
         </div>`,
       basicLightboxOptions
