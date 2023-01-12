7f2cbb35d3dfd8680f117701134dd2256260334d
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-01-12 23:51:32 +0200

    add basicligtbox

--- src/js/film-info-modal.js
+++ src/js/film-info-modal.js
@@ -1,36 +1,47 @@
 import { searchMovieById } from './fetchApi';
+import { buildTrailerBtns } from './trailer-modal';
 
 const basicLightbox = require('basiclightbox');
 const filmCardSection = document.querySelector('.main-section__allcards');
 
+filmCardSection.addEventListener('click', e => {
+  if (e.target.parentNode.className !== 'main-section__card') {
+    return;
+  }
 
-filmCardSection.addEventListener('click', (e) => {
-    if (e.target.parentNode.className !== 'main-section__card') {
-        return
-    };
+  const movieId = 550;
 
-    searchMovieById(77).then(movieInfo => {
-        const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
-        const getGener = movieInfo.genres.map(gener => gener.name).join(', ');
+  searchMovieById(movieId).then(movieInfo => {
+    const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
+    const getGener = movieInfo.genres.map(gener => gener.name).join(', ');
 
-        const createFilmModalMarkup = basicLightbox.create(`
+    const createFilmModalMarkup = basicLightbox.create(`
         <div class="film-modal">
             <div class="film-modal__wrapper">
-                <img class="film-modal__poster" src="${POSTER_URL + movieInfo.poster_path}" alt="Movie ${movieInfo.title} poster" />
+                <img class="film-modal__poster" src="${
+                  POSTER_URL + movieInfo.poster_path
+                }" alt="Movie ${movieInfo.title} poster" />
+               <div id="trailerBtns-wrapper"></div>
                 <div>
                     <h2 class="film-modal__title">${movieInfo.title}</h2>
                     <ul class="film-modal__info-list">
                     <li class="film-modal__info-item">
                         <h3 class="film-modal__info-title">Vote / Votes</h3>
-                        <p class="film-modal__info-text">${movieInfo.vote_average}/${movieInfo.vote_count}</p>
+                        <p class="film-modal__info-text">${
+                          movieInfo.vote_average
+                        }/${movieInfo.vote_count}</p>
                     </li>
                     <li class="film-modal__info-item">
                         <h3 class="film-modal__info-title">Popularity</h3>
-                        <p class="film-modal__info-text">${movieInfo.popularity}</p>
+                        <p class="film-modal__info-text">${
+                          movieInfo.popularity
+                        }</p>
                     </li>
                     <li class="film-modal__info-item">
                         <h3 class="film-modal__info-title">Original Title</h3>
-                        <p class="film-modal__info-text">${movieInfo.original_title}</p>
+                        <p class="film-modal__info-text">${
+                          movieInfo.original_title
+                        }</p>
                     </li>
                     <li class="film-modal__info-item">
                         <h3 class="film-modal__info-title">Genre</h3>
@@ -38,17 +49,18 @@ filmCardSection.addEventListener('click', (e) => {
                     </li>
                     <li>
                         <h3 class="film-modal__info-item--about">About</h3>
-                        <p class="film-modal__info-text--about">${movieInfo.overview}</p>
+                        <p class="film-modal__info-text--about">${
+                          movieInfo.overview
+                        }</p>
                     </li>
                     </ul>
+                    
                 </div>
             </div>
         </div>`);
-        
-        createFilmModalMarkup.show();
-    });
-})
 
+    createFilmModalMarkup.show();
 
-
-
+    buildTrailerBtns(movieId, createFilmModalMarkup);
+  });
+});
--- src/js/trailer-modal.js
+++ src/js/trailer-modal.js
@@ -1,34 +1,53 @@
 import { searchTrailerById } from './fetchApi';
-
+import * as basicLightbox from 'basiclightbox';
 const cardsRef = document.querySelector(`.main-section__allcards`);
 
 // trailerBtn.addEventListener('click', searchTrailerById);
 
-cardsRef.addEventListener('click', onClick);
+// cardsRef.addEventListener('click', onClick);
 
-async function onClick(event) {
-  if (event.target.dataset.movieid) {
-    const movieId = event.target.dataset.movieid;
-    console.log(movieId);
+// async function onClick(event) {
+//   if (event.target.dataset.movieid) {
+//     const movieId = event.target.dataset.movieid;
+//     console.log(movieId);
 
-    buildTrailerBtns(movieId);
-  }
-}
-function showTrailerModal(trailerId) {
-  const trailerModalContainer = document.querySelector('#trailer-modal');
+//     buildTrailerBtns(movieId);
+//   }
+// }
+export function showTrailerModal(trailerId, filmModal) {
+  filmModal.close();
+  const trailerModel = basicLightbox.create(`
+  <div id="trailer-modal" data-modal>
+  <button type="button" class="modal__close" data-modal-close>
+    <svg class="modal__icon" width="18" height="18">
+      <use href="./images/logo.svg#icon-close-black-18dp-2-1"></use>
+    </svg>
+  </button>
+  <iframe
+    id="trailer-iframe"
+    width="560"
+    height="315"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  ></iframe>  
+</div>`);
+
+  trailerModel.show();
 
   const trailerIframe = document.querySelector('#trailer-iframe');
   trailerIframe.src = `https://www.youtube.com/embed/${trailerId}`;
-
-  trailerModalContainer.classList.remove('visually-hidden');
-  console.log(trailerId, trailerModalContainer);
 }
 
-async function buildTrailerBtns(movieId) {
+export async function buildTrailerBtns(movieId, createFilmModalMarkup) {
   const trailers = await searchTrailerById(movieId);
 
   console.log(trailers);
-  const container = document.querySelector(`#trailers-${movieId}`);
+  const container = document.querySelector(`#trailerBtns-wrapper`);
+
+  console.log(container, movieId);
+
   if (trailers.results.length === 0) {
     container.innerHTML = 'No trailers are found';
     return;
@@ -36,7 +55,9 @@ async function buildTrailerBtns(movieId) {
   const trailerButtons = trailers.results.map((item, key) => {
     const button = document.createElement('button');
     button.innerText = `trailer ${key + 1}`;
-    button.addEventListener('click', () => showTrailerModal(item.key));
+    button.addEventListener('click', () =>
+      showTrailerModal(item.key, createFilmModalMarkup)
+    );
 
     return button;
   });
