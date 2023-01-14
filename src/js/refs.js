7d81126a4a140f9240551a282c9e00bcfbe527c1
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-01-14 19:15:31 +0200

    додано js.refs виправлено посилання на постер

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+    mainContainerEl: document.querySelector('.main-section__allcards'),
+    searchForm: document.querySelector('.search'),
+    input_error: document.querySelector('.warning-notification'),
+    
+}
+
