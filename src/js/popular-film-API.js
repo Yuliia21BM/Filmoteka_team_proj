aa0fba9b80c0e94a35fc1857c6911a1c1d564d84
andrewscratch <andrew.chudlya@gmail.com>
2023-01-14 23:22:16 +0200

    пагінація
    правки пагінаціі

--- src/js/popular-film-API.js
+++ src/js/popular-film-API.js
@@ -1,11 +1,16 @@
 import { getPopularFilm } from '../js/fetchApi';
+import * as pagination from './pagination';
 
 import { renderFilmCards } from './render-card';
 
-let currentPage = 1;
+const _getPopularFilm = () => {
+  getPopularFilm(pagination.getCurrentPage()).then(
+    ({ page, results, total_pages, total_results }) => {
+      renderFilmCards(results);
+    }
+  );
+};
 
-getPopularFilm(currentPage).then(
-  ({ page, results, total_pages, total_results }) => {
-    renderFilmCards(results);
-  }
-);
+_getPopularFilm();
+
+pagination.subscribeOnPageChange(_getPopularFilm);
