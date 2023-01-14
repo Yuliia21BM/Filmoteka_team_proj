e0d0e69df490f5e65deefaff466a660ff836d9a4
FilBelan <filbelan@gmail.com>
2023-01-14 16:25:04 +0200

    new code filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -11,48 +11,58 @@ const renderFilmsArea = document.querySelector('.renderFilms');
 
 familyBtn.addEventListener('click', getMovies);
 
-// function rengerGanre(id) {
+// // function rengerGanre(id) {
 
-// }
+// // }
 
-// function getMovies() {
-//     const urlAPI = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
+// // function getMovies() {
+// //     const urlAPI = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
+// //     axios.get(urlAPI)
+// //         .then(res => console.log(res.data.total_results))
+// //         .then(({ results }) => renderFilms(results))
+// //     .catch(error => console.log(error))
+// // }
+
+
+
+// function getMovies(page) {
+//     // page = 1
+//     for (let page = 1; page < 5; page++) {
+//        const urlAPI = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
+//     console.log(urlAPI);
 //     axios.get(urlAPI)
-//         .then(res => console.log(res.data.total_results))
+//         .then(res => {
+//             page += 1;
+    
+//             return res.data
+//         })
 //         .then(({ results }) => renderFilms(results))
+        
+//         // .then(({ results }) => renderFilms(results))
 //     .catch(error => console.log(error))
+//     }
+    
 // }
 
-const page = 1;
-function getMovies() {
-    const urlAPI = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
-    axios.get(urlAPI)
-        .then(res => {
-            // page += 1;
-            console.log(res.data)
-        })
-        
-        .then(({ results }) => renderFilms(results))
-    .catch(error => console.log(error))
-}
+
+// function renderFilms(results) {
+//     renderFilmsArea.innerHTML = '';
+    
+//     const movieElements = results.map(({ poster_path, title, genre_ids }) => {
+//         if(genre_ids.includes(16))
+//         return `<div class="main-section__card">
+//         <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${poster_path}" width="500" height="600" alt="${title}"  loading="lazy">
+//     <p class="main-section__name">
+// 					<h2 class="main-section__card-title">${title}</h2>
+// 					<p class="main-section__description">Drama, Action | 2022</p>
+// 				</p>
+//     </div>`
+//     }).join('');
+//         renderFilmsArea.insertAdjacentHTML('beforeend', movieElements)
+    
+// }
 
 
-function renderFilms(results) {
-    renderFilmsArea.innerHTML = '';
-    
-    const movieElements = results.map(({ poster_path, title, genre_ids }) => {
-        if(genre_ids.includes(28))
-        return `<div class="main-section__card">
-        <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${poster_path}" width="500" height="600" alt="${title}"  loading="lazy">
-    <p class="main-section__name">
-					<h2 class="main-section__card-title">${title}</h2>
-					<p class="main-section__description">Drama, Action | 2022</p>
-				</p>
-    </div>`
-    }).join('');
-        renderFilmsArea.insertAdjacentHTML('beforeend', movieElements)
-    
-}
 
 
 
