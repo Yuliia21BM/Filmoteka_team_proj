d6287fd9f2b12a08d8e913bee80232b1d7654645
Sergey <sergey.cherenkov96@gmail.com>
2023-01-14 20:31:51 +0200

    add def poster

---
+++ src/js/create-images-for-js-input.js
@@ -0,0 +1,20 @@
+let iconCross = document.createElement('img');
+iconCross.src = new URL(
+  '/src/images/svg/close-modal-film-icon.svg',
+  import.meta.url
+);
+
+
+
+let defaultPoster = document.createElement('img');
+defaultPoster.src = new URL(
+  '/src/images/default-poster.jpg',
+  import.meta.url
+);
+
+console.log(defaultPoster);
+
+export {
+    iconCross,
+    defaultPoster,
+};
--- src/js/film-info-modal.js
+++ src/js/film-info-modal.js
@@ -1,14 +1,12 @@
 import { searchMovieById } from './fetchApi';
 import { addWatch, addQueue } from './localstorage-save-films-API';
+import { iconCross, defaultPoster } from './create-images-for-js-input';
 
 const basicLightbox = require('basiclightbox');
 const filmCardSection = document.querySelector('.main-section__allcards');
 
-let iconCross = document.createElement('img');
-iconCross.src = new URL(
-  '/src/images/svg/close-modal-film-icon.svg',
-  import.meta.url
-);
+
+
 
 filmCardSection.addEventListener('click', e => {
   openModal(e, 'main-section__card');
@@ -21,10 +19,12 @@ export function openModal(e, childClass) {
 
   const filmId = e.target.parentNode.dataset.filmId;
 
-  searchMovieById(filmId).then(movieInfo => {
+
+  searchMovieById(filmId).then (({poster_path, title, genres, vote_count, vote_average, popularity, original_title, overview}) => {
+    
     const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 
-    const getGener = movieInfo.genres.map(gener => gener.name).join(', ');
+    const getGener = genres.map(gener => gener.name).join(', ');
 
     const basicLightboxOptions = {
       onShow: () => {
@@ -44,33 +44,25 @@ export function openModal(e, childClass) {
     const createFilmModalMarkup = basicLightbox.create(
       `
         <div class="film-modal">
-            <button class="film-modal__close-btn" type="button"></button>
-                <img class="film-modal__poster" src="${
-                  POSTER_URL + movieInfo.poster_path
-                }" alt="Movie ${movieInfo.title} poster" />
+                <button class="film-modal__close-btn" type="button"></button>
+                <div class="film-modal__poster-wrapper"><img class="film-modal__poster" src="${poster_path ? POSTER_URL + poster_path : ''}" alt="Movie ${title} poster" /></div>
                 <div class="film-modal__wrapper">
                   <div class="film-modal__info-wrapper">
-                    <h2 class="film-modal__title">${movieInfo.title}</h2>
+                    <h2 class="film-modal__title">${title}</h2>
                     <ul class="film-modal__info-list">
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Vote/Votes</h3>
-                            <p class="film-modal__info-text"><span class="film-modal__info-text--vote">${movieInfo.vote_average.toFixed(
+                            <p class="film-modal__info-text"><span class="film-modal__info-text--vote">${vote_average.toFixed(
                               1
-                            )}</span>  <span class="film-modal__info-text--slash">/</span> <span class="film-modal__info-text--vote film-modal__info-text--vote-count">${
-        movieInfo.vote_count
-      }</p></span>
+                            )}</span>  <span class="film-modal__info-text--slash">/</span> <span class="film-modal__info-text--vote film-modal__info-text--vote-count">${vote_count}</p></span>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Popularity</h3>
-                            <p class="film-modal__info-text">${
-                              movieInfo.popularity
-                            }</p>
+                            <p class="film-modal__info-text">${popularity}</p>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Original Title</h3>
-                            <p class="film-modal__info-text">${
-                              movieInfo.original_title
-                            }</p>
+                            <p class="film-modal__info-text">${original_title}</p>
                         </li>
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Genre</h3>
@@ -79,19 +71,19 @@ export function openModal(e, childClass) {
                         <li class="film-modal__info-item">
                             <h3 class="film-modal__info-title">Trailer</h3>
                             <p class="film-modal__info-text"><button class="film-modal__trailer-btn" type="button" data-id="${
-                              movieInfo.id
+                              filmId
                             }">Watch trailer<span class="triangle-right"></span></button></p>
                         </li>
                     </ul>
                     <h3 class="film-modal__title-about">About</h3>
-                    <p class="film-modal__text-about">${movieInfo.overview}</p>
+                    <p class="film-modal__text-about">${overview}</p>
                     </div>  
                 <div class="film-modal__add-btns-wrapper">
                   <button class="film-modal__add-btns btn-add-watched accent-btn trailer-btn" type="button" data-id="${
-                    movieInfo.id
+                    filmId
                   }">Add to Watched</button>
                   <button class="film-modal__add-btns btn-add-queue film-modal__add-btns--seconadry-btn second-btn" type="button" data-id="${
-                    movieInfo.id
+                    filmId
                   }">Add to queue</button>
                 </div>
                 </div>
@@ -100,6 +92,14 @@ export function openModal(e, childClass) {
     );
 
     createFilmModalMarkup.show();
+
+    if (!poster_path) {
+      document.querySelector('.film-modal__poster').remove();
+      document.querySelector('.film-modal__poster-wrapper').appendChild(defaultPoster).classList.add('film-modal__poster');
+    }
+
+
+
 
     if (createFilmModalMarkup.visible()) {
       const watchBtn = document.querySelector('button.btn-add-watched');
