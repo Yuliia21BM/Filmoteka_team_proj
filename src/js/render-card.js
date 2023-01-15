1bff4073cef7d79456cbaeb6bd587a8d16d2084c
TaraniukD <taranyk88@ukr.net>
2023-01-15 22:53:33 +0200

    update card

--- src/js/render-card.js
+++ src/js/render-card.js
@@ -24,44 +24,61 @@ export function createElementsMovie(data) {
 
   const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
   let genreFilm = '';
-  let img = document.createElement('img');
-
-  img.src = new URL('../images/default-poster.jpg', import.meta.url);
-
 
   const markupFromAPI = () => `
-  <div class="main-section__card" data-film-id="${id}">
-        <img src="${poster_path ? POSTER_URL + poster_path : img.src}" alt="${
-    title ? title : 'Unknown'
+  <li class="main-section__card-conteiner">
+  <div class="main-section__card main-section__popup" data-film-id="${data.id}">
+        <img src="${
+          data.poster_path ? POSTER_URL + data.poster_path : defaultPoster.src
+        }" alt="${
+    data.title ? data.title : 'Unknown'
   }" class="main-section__image" loading="lazy">
-    <div>
-					<h2 class="main-section__card-title">${title ? title : 'Unknown title'}</h2>
-          <div class="main-section__cards">
-					<p class="main-section__description">${genre_ids ? genreFilm : 'Unknown'} | ${
-    release_date ? release_date.slice(0, 4) : 'n/a'
+  <p class="main-section__popup-title"> <span class = "popUp-title">About</span> <br/>${
+    data.overview ? data.overview : 'No description'
   }</p>
+  </div>
+      <div class="main-section__card main-section__cards" data-film-id="${data.id}">
+					<h2 class="main-section__card-title">${
+            data.title ? data.title : 'Unknown title'
+          }</h2>
+                   
+					<p class="main-section__description">${
+            data.genre_ids ? genreFilm : 'Unknown'
+          } | ${data.release_date ? data.release_date.slice(0, 4) : 'n/a'}
+				
                 <span class="main-section__card-rating">${
-                  vote_average ? vote_average.toFixed(1) : '0'
-                }</span></div>
-                </div>
+                  data.vote_average ? data.vote_average.toFixed(1) : '0.0'
+                }</span>
+                </p>
+      </div>             
     </div>`;
 
   const markupFromStorage = () => `
-  <div class="main-section__card" data-film-id="${id}">
-        <img src="${poster_path ? POSTER_URL + poster_path : img.src}" alt="${
-    title ? title : 'Unknown'
+  <li class="main-section__card-conteiner">
+  <div class="main-section__card main-section__popup" data-film-id="${data.id}">
+        <img src="${
+          data.poster_path ? POSTER_URL + data.poster_path : defaultPoster.src
+        }" alt="${
+    data.title ? data.title : 'Unknown'
   }" class="main-section__image" loading="lazy">
-    <div>
-					<h2 class="main-section__card-title">${title ? title : 'Unknown title'}</h2>
-                    <div class="main-section__cards">
-					<p class="main-section__description">${genre_ids ? genreFilm : data.genres} | ${
-    release_date ? release_date.slice(0, 4) : 'n/a'
+  <p class="main-section__popup-title"> <span class = "popUp-title">About</span> <br/>${
+    data.overview ? data.overview : 'No description'
   }</p>
+  </div>
+      <div class="main-section__card main-section__cards" data-film-id="${data.id}">
+					<h2 class="main-section__card-title">${
+            data.title ? data.title : 'Unknown title'
+          }</h2>
+                   
+					<p class="main-section__description">${
+            data.genre_ids ? genreFilm : 'Unknown'
+          } | ${data.release_date ? data.release_date.slice(0, 4) : 'n/a'}
+				
                 <span class="main-section__card-rating">${
-
-                  vote_average ? vote_average.toFixed(1) : '0'
-                }</span></div>
-                </div>
+                  data.vote_average ? data.vote_average.toFixed(1) : '0.0'
+                }</span>
+                </p>
+      </div>             
     </div>`;
 
   if (!genre_ids) {
