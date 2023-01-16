0eb4e9ff7e897cd049569cdc95c07923e59eb18a
yarqui <108986486+yarqui@users.noreply.github.com>
2023-01-16 13:57:01 +0200

    1

--- src/js/library-queue-list.js
+++ src/js/library-queue-list.js
@@ -8,7 +8,7 @@ const movie = [
     title: 'Test Movie1',
     genres: 'Drama, Family, Thriller',
     poster_path: '/1NqwE6LP9IEdOZ57NCT51ftHtWT.jpg',
-    vote_average: 6.2,
+    vote_average: '6.2',
     release_date: '2011-11-02',
   },
   {
@@ -16,11 +16,19 @@ const movie = [
     title: 'Test Movie2',
     genres: 'Comedy, Family, Romance',
     poster_path: '/7CNCv9uhqdwK7Fv4bR4nmDysnd9.jpg',
-    vote_average: 7.8,
+    vote_average: '7.8',
+    release_date: '2008-11-02',
+  },
+  {
+    id: 2,
+    title: 'Test Movie2',
+    genres: 'Comedy, Family, Romance',
+    poster_path: '/7CNCv9uhqdwK7Fv4bR4nmDysnd9.jpg',
+    vote_average: '7.8',
     release_date: '2008-11-02',
   },
 ];
-localStorage.setItem(QUEUE_LIST, JSON.stringify(movie));
+// localStorage.setItem(QUEUE_LIST, JSON.stringify(movie));
 // localStorage.removeItem(QUEUE_LIST);
 // -------------☝Це mockup data, поки немає функціоналу додавання в localStorage -------------
 
