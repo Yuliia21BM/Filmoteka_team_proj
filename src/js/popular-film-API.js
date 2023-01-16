849fbd9acc094fbbf26d8f601e49d7fdcf7ee5bc
Julia Meshchaninova <j.meschaninova@gmail.com>
2023-01-16 22:14:00 +0200

    add skeleton

--- src/js/popular-film-API.js
+++ src/js/popular-film-API.js
@@ -12,7 +12,30 @@ const _getPopularFilm = () => {
     }
   );
 };
+const cardsContaier = document.querySelector('.main-section__allcards');
+let cardsTemplate = `<li class="cards-skeleton">
+        <div class="movie--isloading">
+          <div class="loading-image"></div>
+          <div class="loading-content">
+            <div class="loading-text-container">
+              <div class="loading-main-text"></div>
+              <div class="loading-sub-text"></div>
+            </div>
+            <div class="loading-btn"></div>
+          </div>
+        </div>
+        </li>`;
+const cardsSkeleton = '';
+const renderSkeleton = () => {
+  for (let i = 1; i <= 20; i+=1) {
+    console.log('sdfs');
+    cardsTemplate += cardsTemplate;
+  }
+  cardsContaier.innerHTML = cardsTemplate;
+  return;
+}
 
+renderSkeleton();
 _getPopularFilm();
 
 pagination.subscribeOnPageChange(_getPopularFilm);
