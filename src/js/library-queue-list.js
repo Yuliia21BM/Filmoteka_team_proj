f1ec5e628a957a18469f5a961496f69517ba4040
yarqui <108986486+yarqui@users.noreply.github.com>
2023-01-16 23:18:08 +0200

    working on lib

--- src/js/library-queue-list.js
+++ src/js/library-queue-list.js
@@ -1,5 +1,6 @@
-import { QUEUE_LIST } from './config';
+import { QUEUE_LIST, WATCHED_LIST } from './config';
 import { loadFromStorage } from './localstorage-load-films';
+import { refs } from './refs';
 
 // -------------👇 Для тестів ------------
 // localStorage.setItem(QUEUE_LIST, JSON.stringify(movie));
@@ -7,5 +8,22 @@ import { loadFromStorage } from './localstorage-load-films';
 // -------------☝ Для тестів -------------
 
 // STARTS HERE <================================================================<<<<
+
+const onWatchedBtnClick = () => {
+  loadFromStorage(WATCHED_LIST);
+
+  refs.queueBtn.classList.toggle('header-lib__btn--current');
+  refs.watchedBtn.classList.toggle('header-lib__btn--current');
+};
+
+const onQueueBtnClick = () => {
+  loadFromStorage(QUEUE_LIST);
+
+  refs.watchedBtn.classList.toggle('header-lib__btn--current');
+  refs.queueBtn.classList.toggle('header-lib__btn--current');
+};
+
+refs.watchedBtn.addEventListener('click', onWatchedBtnClick);
+refs.queueBtn.addEventListener('click', onQueueBtnClick);
 
 loadFromStorage(QUEUE_LIST);
