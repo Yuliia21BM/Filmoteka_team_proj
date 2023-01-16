996a8c69fdcbdd8e2381816fe6d5707612668e61
yarqui <108986486+yarqui@users.noreply.github.com>
2023-01-16 16:14:08 +0200

    trying to fix render cards from storage

--- src/js/library-queue-list.js
+++ src/js/library-queue-list.js
@@ -1,36 +1,10 @@
 import { QUEUE_LIST } from './config';
 import { loadFromStorage } from './localstorage-load-films';
 
-// -------------👇Це mockup data, поки немає функціоналу додавання в localStorage ------------
-const movie = [
-  {
-    id: 13,
-    title: 'Test Movie1',
-    genres: 'Drama, Family, Thriller',
-    poster_path: '/1NqwE6LP9IEdOZ57NCT51ftHtWT.jpg',
-    vote_average: '6.2',
-    release_date: '2011-11-02',
-  },
-  {
-    id: 2,
-    title: 'Test Movie2',
-    genres: 'Comedy, Family, Romance',
-    poster_path: '/7CNCv9uhqdwK7Fv4bR4nmDysnd9.jpg',
-    vote_average: '7.8',
-    release_date: '2008-11-02',
-  },
-  {
-    id: 2,
-    title: 'Test Movie2',
-    genres: 'Comedy, Family, Romance',
-    poster_path: '/7CNCv9uhqdwK7Fv4bR4nmDysnd9.jpg',
-    vote_average: '7.8',
-    release_date: '2008-11-02',
-  },
-];
+// -------------👇 Для тестів ------------
 // localStorage.setItem(QUEUE_LIST, JSON.stringify(movie));
 // localStorage.removeItem(QUEUE_LIST);
-// -------------☝Це mockup data, поки немає функціоналу додавання в localStorage -------------
+// -------------☝ Для тестів -------------
 
 // STARTS HERE <================================================================<<<<
 
--- src/js/localstorage-load-films.js
+++ src/js/localstorage-load-films.js
@@ -1,19 +1,19 @@
 import { renderFilmCards, renderNoMoviesMarkup } from './render-card';
 
 // Returns parsed data from Local Storage
-export const loadFromStorage = key => {
+export const loadFromStorage = async key => {
   try {
     const savedData = localStorage.getItem(key);
 
     // If there are no movies added to list in Local Storage, show Travolta GIF from Pulp Fiction
-    if (!savedData) {
+    if (!savedData || savedData.length <= 2) {
       renderNoMoviesMarkup();
       return;
     }
 
-    let movieList = JSON.parse(savedData);
+    let movieList = await JSON.parse(savedData);
 
-    renderFilmCards(movieList);
+    return renderFilmCards(movieList);
   } catch (error) {
     console.error('Error: ', error.message);
   }
