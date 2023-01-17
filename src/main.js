5c3142ccd6c81cd36b65dc3ad5acd3b76e5f9bde
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-17 12:55:47 +0200

    remove pagination on lib

--- src/main.js
+++ src/main.js
@@ -3,5 +3,4 @@ import './js/upcoming-films';
 import './js/film-search-header';
 import './js/filter';
 import './js/popular-film-API';
-
-import './js/authorization-modal';
+import './js/pagination';
