35894f6d39b2c5f1daf2e8d60eb0802e4f8a3df2
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-19 01:05:43 +0200

    Update team-modal.js

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,36 +1,26 @@
-
 (() => {
-
-    const refs = {
-      openModalBtn: document.querySelector('[data-team-modal-open]'),
-      closeModalBtn: document.querySelector('[data-team-modal-close-btn]'),
-      closeModal: document.querySelector('[data-team-modal-close]'),
-      modal: document.querySelector('[data-team-modal]'),
-      body: document.querySelector('body'),
+  const refs = {
+    openModalBtn: document.querySelector('[data-team-modal-open]'),
+    closeModalBtn: document.querySelector('[data-team-modal-close-btn]'),
+    closeModal: document.querySelector('[data-team-modal-close]'),
+    modal: document.querySelector('[data-team-modal]'),
+    body: document.querySelector('body'),
   };
-  
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-    refs.closeModal.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModal.addEventListener('click', toggleModal);
   document.addEventListener('keydown', escClose);
-  
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden-team');
-      refs.body.classList.toggle('no-scroll');
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-team');
+    refs.body.classList.toggle('no-scroll');
   }
-  
+
   function escClose(e) {
-  if (e.key === 'Escape') {
-    refs.modal.classList.add('is-hidden-team');
-    refs.modal.classList.add('no-scroll');
+    if (e.key === 'Escape') {
+      refs.modal.classList.add('is-hidden-team');
+      refs.body.classList.remove('no-scroll');
+    }
   }
-  }
-
-
-  })();
-
- 
-
- 
+})();
