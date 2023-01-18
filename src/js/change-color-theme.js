b945f9abc7eb2b3c1d5d7d239979a258b05c6ab1
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-18 16:47:09 +0200

    Add dark paginination

--- src/js/change-color-theme.js
+++ src/js/change-color-theme.js
@@ -2,11 +2,12 @@ import { THEME } from './config.js';
 
 const iconMoon = document.querySelector('.icon-moon');
 const iconSun = document.querySelector('.icon-sun');
-const cartTitle = document.querySelectorAll('.main-section__card-title');
 
 export function changeColorThemeMain(darkTheme) {
   const cartTitle = document.querySelectorAll('.main-section__card-title');
   const genreBtn = document.querySelectorAll('.genres-button');
+  const paginBtn = document.querySelectorAll('.pagination-button');
+  const dots = document.querySelectorAll('.dots');
 
   if (!darkTheme) {
     iconMoon.style.opacity = 1;
@@ -22,6 +23,26 @@ export function changeColorThemeMain(darkTheme) {
     genreBtn.forEach(el => {
       el.style.borderColor = '#000';
     });
+    paginBtn.forEach(el => {
+      if (
+        el.classList.contains('pagination--current') ||
+        el.classList.contains('arrow-left') ||
+        el.classList.contains('arrow-right')
+      )
+        return;
+      // if (
+      //   el.classList.contains('arrow-left') ||
+      //   el.classList.contains('arrow-right')
+      // ) {
+      //   el.style.fill = '#000';
+      // }
+      el.style.backgroundColor = '#fff';
+      el.style.color = '#000';
+      el.style.border = 'none';
+    });
+    dots.forEach(el => {
+      el.style.color = '#000';
+    });
   } else {
     iconMoon.style.opacity = 0;
     iconSun.style.opacity = 1;
@@ -34,6 +55,27 @@ export function changeColorThemeMain(darkTheme) {
     });
     genreBtn.forEach(el => {
       el.style.borderColor = '#fff';
+    });
+    paginBtn.forEach(el => {
+      if (
+        el.classList.contains('pagination--current') ||
+        el.classList.contains('arrow-left') ||
+        el.classList.contains('arrow-right')
+      )
+        return;
+      // if (
+      //   el.classList.contains('arrow-left') ||
+      //   el.classList.contains('arrow-right')
+      // ) {
+      //   el.style.fill = '#fff';
+      // }
+      el.style.backgroundColor = '#181F25';
+      el.style.color = '#fff';
+      el.style.border = '1px solid #fff';
+      el.style.borderRadius = '5px';
+    });
+    dots.forEach(el => {
+      el.style.color = '#fff';
     });
   }
 }
