3d54a563beb0886cc72c6f34ac8d61fa78a7ed99
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-18 17:24:20 +0200

    Update change-color-theme.js

--- src/js/change-color-theme.js
+++ src/js/change-color-theme.js
@@ -2,11 +2,12 @@ import { THEME } from './config.js';
 
 const iconMoon = document.querySelector('.icon-moon');
 const iconSun = document.querySelector('.icon-sun');
-const cardTitle = document.querySelectorAll('.main-section__card-title');
 
 export function changeColorThemeMain(darkTheme) {
   const cardTitle = document.querySelectorAll('.main-section__card-title');
   const genreBtn = document.querySelectorAll('.genres-button');
+  const paginBtn = document.querySelectorAll('.pagination-button');
+  const dots = document.querySelectorAll('.dots');
 
   if (!darkTheme) {
     iconMoon.style.opacity = 1;
@@ -24,36 +25,6 @@ export function changeColorThemeMain(darkTheme) {
     // genreBtn.forEach(el => {
     //   el.style.borderColor = '#000';
     // });
-  } else {
-    iconMoon.style.opacity = 0;
-    iconSun.style.opacity = 1;
-    document.body.style.backgroundColor = '#181F25';
-    cardTitle.forEach(el => {
-      el.style.color = '#fff';
-    });
-    genreBtn.forEach(el => el.classList.add('genres-button--dark-theme'));
-    // genreBtn.forEach(el => {
-    //   el.style.color = '#fff';
-    // });
-    // genreBtn.forEach(el => {
-    //   el.style.borderColor = '#fff';
-    // });
-  }
-}
-
-export function changeColorThemeLib(darkTheme) {
-  const cardTitle = document.querySelectorAll('.main-section__card-title');
-  const paginBtn = document.querySelectorAll('.pagination-button');
-  const dots = document.querySelectorAll('.dots');
-
-  if (!darkTheme) {
-    iconMoon.style.opacity = 1;
-    iconSun.style.opacity = 0;
-    document.body.style.backgroundColor = '#fff';
-    // console.log(cardTitle);
-    cardTitle.forEach(el => {
-      el.style.color = '#000';
-    });
     paginBtn.forEach(el => {
       if (
         el.classList.contains('pagination--current') ||
@@ -71,10 +42,17 @@ export function changeColorThemeLib(darkTheme) {
   } else {
     iconMoon.style.opacity = 0;
     iconSun.style.opacity = 1;
-    document.body.style.backgroundColor = '#000';
+    document.body.style.backgroundColor = '#181F25';
     cardTitle.forEach(el => {
       el.style.color = '#fff';
     });
+    genreBtn.forEach(el => el.classList.add('genres-button--dark-theme'));
+    // genreBtn.forEach(el => {
+    //   el.style.color = '#fff';
+    // });
+    // genreBtn.forEach(el => {
+    //   el.style.borderColor = '#fff';
+    // });
     paginBtn.forEach(el => {
       if (
         el.classList.contains('pagination--current') ||
@@ -88,6 +66,27 @@ export function changeColorThemeLib(darkTheme) {
       el.style.borderRadius = '5px';
     });
     dots.forEach(el => {
+      el.style.color = '#fff';
+    });
+  }
+}
+
+export function changeColorThemeLib(darkTheme) {
+  const cardTitle = document.querySelectorAll('.main-section__card-title');
+
+  if (!darkTheme) {
+    iconMoon.style.opacity = 1;
+    iconSun.style.opacity = 0;
+    document.body.style.backgroundColor = '#fff';
+    // console.log(cardTitle);
+    cardTitle.forEach(el => {
+      el.style.color = '#000';
+    });
+  } else {
+    iconMoon.style.opacity = 0;
+    iconSun.style.opacity = 1;
+    document.body.style.backgroundColor = '#000';
+    cardTitle.forEach(el => {
       el.style.color = '#fff';
     });
   }
