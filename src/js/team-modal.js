e066a8d8aabbf6eb8a0408954ea4c26ea6167594
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-01-19 00:32:20 +0200

    team-modal-close

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,13 +1,15 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-team-modal-open]'),
-      closeModalBtn: document.querySelector('[data-team-modal-close]'),
+      closeModalBtn: document.querySelector('[data-team-modal-close-btn]'),
+      closeModal: document.querySelector('[data-team-modal-close]'),
       modal: document.querySelector('[data-team-modal]'),
       body: document.querySelector('body'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeModal.addEventListener('click', toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden-team');
