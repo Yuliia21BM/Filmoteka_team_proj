c71d20ddccbd7f01c0413a3fa9c951490056a9ba
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-01-18 10:06:23 +0200

    team-modal-design

---
+++ src/js/team-modal-swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(
+    '.team-swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
