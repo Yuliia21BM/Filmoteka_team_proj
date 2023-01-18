7946398c80799282ab14739a2aff7c0fb69f8141
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-01-18 11:36:18 +0200

    team-modal-swiper-bags

--- src/js/team-modal-swiper.js
+++ src/js/team-modal-swiper.js
@@ -1,12 +1,20 @@
 const swiper = new Swiper(
     '.team-swiper', {
     direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 20,
     loop: true,
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+
+  speed: 2500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
   },
 
 });
