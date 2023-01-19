afc85572d096c59b3e74742a964ddd19b0545d53
yarqui <108986486+yarqui@users.noreply.github.com>
2023-01-19 12:27:04 +0200

    tuned search input UX

--- src/js/film-search-header.js
+++ src/js/film-search-header.js
@@ -9,51 +9,58 @@ const error = document.querySelector('.warning-notification');
 refs.searchForm.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit(e) {
-    e.preventDefault();
-    refs.mainContainerEl.scrollIntoView({ behavior: 'smooth' });
+  e.preventDefault();
+  refs.searchInput.blur();
+  refs.mainContainerEl.scrollIntoView({ behavior: 'smooth' });
   const value = e.currentTarget.searchQuery.value.trim();
-   
-    await getMoviesHandler(value) 
-    pagination.setCurrentPageto1()
-    pagination.subscribeOnPageChange(() => {
-    getMoviesHandler(refs.searchInput.value)
+
+  await getMoviesHandler(value);
+  pagination.setCurrentPageto1();
+  pagination.subscribeOnPageChange(() => {
+    getMoviesHandler(refs.searchInput.value);
 
     // console.log(input.value, input.text, 'subscriberTextContent')
-});    
-    
-    };
+  });
+}
 
 async function getMoviesHandler(value) {
-    if (value === '') { 
+  if (value === '') {
+    return (error.textContent =
+      'No matches found for your query. Enter the correct movie name.');
+  } else error.textContent = '';
 
-        return (error.textContent =
-            'No matches found for your query. Enter the correct movie name.');
+  try {
+    const response = await searchMovieByName(
+      value,
+      pagination.getCurrentPage()
+    );
+    pagination.setTotalPages(response.total_pages);
+    const getMovie = response.results;
+    // console.log(response);
+    // console.log(getMovie);
+
+    if (getMovie.length === 0) {
+      return (
+        (error.textContent = `No matches found for your query. Enter the correct movie name.`),
+        (refs.mainContainerEl.innerHTML = ' '),
+        (refs.mainContainerEl.innerHTML = `<div class="wrong-box"> <p class="not-found-text">Not Found</p></div>`),
+        document
+          .querySelector('.wrong-box')
+          .prepend(notFoundFilm)
+          .classList.add('film-not-found'),
+        document
+          .querySelector('.pagination-container')
+          .classList.add('visually-hidden')
+      );
+    } else {
+      (error.textContent = ''),
+        document
+          .querySelector('.pagination-container')
+          .classList.remove('visually-hidden');
     }
-    else error.textContent = '';
 
-    try {
-        const response = await searchMovieByName(value, pagination.getCurrentPage());
-        pagination.setTotalPages(response.total_pages)
-        const getMovie = response.results;
-        // console.log(response);
-        // console.log(getMovie);
-
-        if (getMovie.length === 0) {
-
-            return (error.textContent = `No matches found for your query. Enter the correct movie name.`),
-                refs.mainContainerEl.innerHTML = " ",
-                refs.mainContainerEl.innerHTML = `<div class="wrong-box"> <p class="not-found-text">Not Found</p></div>`,
-                document.querySelector('.wrong-box').prepend(notFoundFilm).classList.add('film-not-found'),
-                document.querySelector('.pagination-container').classList.add("visually-hidden");       
-    }
-        else {
-            error.textContent = '',
-            document.querySelector('.pagination-container').classList.remove("visually-hidden");    
-        };
-
-        renderFilmCards(getMovie);
-
-    } catch (error) {
-        console.log(error);
-    }
-};
+    renderFilmCards(getMovie);
+  } catch (error) {
+    console.log(error);
+  }
+}
